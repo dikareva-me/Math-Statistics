a6b725ffb5c8d1b386717d2da550505d5cc874b1
dikareva-me <mary1voight@gmail.com>
2023-06-04 17:05:02 +0300

    Main implemented

---
+++ lab4/main.m
@@ -0,0 +1,17 @@
+pkg load interval
+
+input = csvread("Channel_1_500nm_0.23mm.csv")
+eps = 1e-4
+
+input_int = infsup(input - eps, input + eps)
+
+data;
+data_regr;
+data_regr_reduce;
+omegas;
+regr_residuals;
+regr_reduce_residuals;
+mode_freq;
+inform_set;
+corridor_of_joint_dep;
+
